2c2032fb34ce071ff1d2511f0ce682f13e04b588
QCdragon <qcdreamers@outlook.com>
2022-12-03 22:28:57 +0800

    rewrote parts of the comparator, cleaned up

--- Timers/Timer.h
+++ Timers/Timer.h
@@ -11,10 +11,11 @@ namespace Timers {
 		std::chrono::steady_clock::time_point timeStart, timeEnd;
 		std::chrono::duration<double> dur;
 	public:
-		autoTimer() {
+		autoTimer()	{
 			timeStart = std::chrono::high_resolution_clock::now();
 		}
-		~autoTimer() {
+		~autoTimer() 
+		{
 				timeEnd = std::chrono::high_resolution_clock::now();
 				dur = timeStart - timeEnd;
 				printf("proccess used %lf ms \n", (dur.count() * 1000));
@@ -32,11 +33,32 @@ namespace Timers {
 			start = true;
 			timeStart = std::chrono::high_resolution_clock::now();
 		}
+		double interval() { //does not reset
+			if (!start) printf("the timer was not started");
+			timeEnd = std::chrono::high_resolution_clock::now();
+			dur = timeEnd - timeStart;
+			printf("proccess used %lf ms \n", (dur.count() * 1000));
+			return dur.count() * 1000;
+		}
+		double intervalNoOutput() {
+			if (!start) printf("the timer was not started");
+			timeEnd = std::chrono::high_resolution_clock::now();
+			dur = timeEnd - timeStart;
+			return dur.count() * 1000;
+		}
 		double endTimer() {
 			if (!start) printf("the timer was not started");
 			timeEnd = std::chrono::high_resolution_clock::now();
 			dur = timeEnd - timeStart;
 			printf("proccess used %lf ms \n", (dur.count() * 1000));
+			start = false; //can be reused, resets
+			return dur.count() * 1000;
+		}
+		double endTimerNoOutput() {
+			if (!start) printf("the timer was not started");
+			timeEnd = std::chrono::high_resolution_clock::now();
+			dur = timeEnd - timeStart;
+			start = false; //can be reused, resets
 			return dur.count() * 1000;
 		}
 	};
@@ -46,6 +68,14 @@ namespace Timers {
 	class Comparator { //takes function pointers
 	private:
 		manualTimer timer;
+		/*
+		struct process{
+			reType(*)(Ts...) p_func;
+			Ts... func_Args;
+			double Time;
+			std::thread rthread
+		};
+		std::vector<process> processes; */
 
 		std::vector<reType(*)(Ts...)> funcs; //vector of all functions
 		std::vector<Ts...> funcArgs;
@@ -56,7 +86,7 @@ namespace Timers {
 			manualTimer ptimer;
 			ptimer.startTimer();
 			funcs[funcs.size() - address - 1](funcArgs[funcs.size() - address - 1]); //in the correct order
-			Times.emplace_back(ptimer.endTimer());
+			Times.emplace_back(ptimer.endTimerNoOutput());
 		}
 	public:
 		void addFunc(reType(*func)(Ts...), Ts... arg) { 
@@ -67,12 +97,15 @@ namespace Timers {
 			for (int i = 0; i < funcs.size(); i++)
 				threads.push_back(std::thread(&Comparator::parallelFunc, this, i));
 			for (auto& t : threads) t.join();
+			for (int i = 1; i <= Times.size(); i++) {
+				printf("process number %i took %lf milliseconds\n", i, Times[i-1]);
+			}
 		}
 		void const run() {
 			for (int i = 0; i < funcs.size(); i++) {
 				timer.startTimer();
 				funcs[i](funcArgs[i]);
-				Times.emplace_back(timer.endTimer());
+				Times.emplace_back(timer.endTimerNoOutput());
 			}
 		}
 		std::vector<double> const getTimes() {
